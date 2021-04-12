bf44b544844b03804f976677122d5c75bf2430f2
Wenbin Liu <wenbin_liu@outlook.com>
2021-04-12 17:46:37 +0800

    更改了init-yasnippet的排版

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,13 +1,15 @@
+;;; init-yasnippet.el -- support for yasnippet
+;;; Commentary:
+;;; Code:
 (use-package yasnippet
-             :ensure t
-             :init (add-to-list 'load-path
-                                "~/.emacs.d/snippet")
-             :config
-             (yas-global-mode 1)
-             (use-package yasnippet-snippets
-                          :ensure t)
-             ）
+  :ensure t
+  :init (add-to-list 'load-path
+                     "~/.emacs.d/snippet")
+  :config
+  (yas-global-mode 1)
+  (use-package yasnippet-snippets
+    :ensure t))
 
 
 (provide 'init-yasnippet)
-;;; init-yasnippet ends here
+;;; init-yasnippet.el ends here
